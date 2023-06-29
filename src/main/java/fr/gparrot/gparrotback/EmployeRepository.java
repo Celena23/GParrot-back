8064858ac6467dd156a1aab4a203b7931f177817
piatce <ce.piat@sources-alma.com>
2023-06-29 17:12:03 +0200

    EmployeRepository

---
+++ src/main/java/fr/gparrot/gparrotback/EmployeRepository.java
@@ -0,0 +1,17 @@
+package fr.gparrot.gparrotback;
+
+
+import fr.gparrot.gparrotback.entities.Employe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+/**
+ * Repository de la Class Employe. Va permettre d'interroger la bdd
+ */
+
+
+@RepositoryRestResource(collectionResourceRel = "employe", path = "employe")
+@CrossOrigin(origins="*")
+public interface EmployeRepository extends JpaRepository<Employe, Long> {
+}
--- src/main/java/fr/gparrot/gparrotback/entities/Employe.java
+++ src/main/java/fr/gparrot/gparrotback/entities/Employe.java
@@ -1,7 +1,6 @@
 package fr.gparrot.gparrotback.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,6 +11,7 @@ import lombok.ToString;
 @Entity
 public class Employe {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String firstname;
